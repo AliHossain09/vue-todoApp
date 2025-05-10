4ec15b8043a8a11867fa8f9ea5b5dbd1ff2d1cde
AliHossain09 <alihoossainn@gmail.com>
2025-05-10 18:31:07 +0600

    complete todoApp project with vueJs

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,43 @@
 export default {
  data() {
   return{
-    
-    
-  }
+    tasks: [],
+    taskInput: null,
+    isSearch: false,
+    serchInput: null,
+    }
  },
- 
- 
+
+    methods: {
+      addTask() {
+        this.tasks.push({
+          value: this.taskInput,
+          isComplete: false,
+        })
+        this.taskInput = null
+      },
+      removeTask(index) {
+        this.tasks.splice(index, 1)
+      },
+      clearAll() {
+        this.tasks = []
+      },
+      closeSerch() {
+        this.serchInput = null,
+        this.isSearch = false
+      }
+    },
+    computed: {
+  tasksList() {
+    if (this.serchInput) {
+      return this.tasks.filter(data => data.value.indexOf(this.serchInput) !== -1)
+    } else {
+      return this.tasks;
+    }
   }
+ 
+}
+}
  
   
   
@@ -30,8 +60,8 @@ export default {
     <!-- List box -->
     <div class="bg-white p-8 w-full max-w-xl rounded">
       <!-- Add task -->
-      <form class="flex">
-        <input
+      <form @submit.prevent="addTask()" class="flex">
+        <input v-model="taskInput"
             type="text"
             class="block w-full border border-gray-300 focus:border-gray-400 focus:outline-none rounded-l-md px-4 py-1.5"
             placeholder="Add task to your list"/>
@@ -48,7 +78,7 @@ export default {
         <h3 class="text-gray-600">Your tasks:</h3>
         <div class="flex items-center h-8">
           <!-- Search -->
-          <button>
+          <button v-show="!isSearch" @click="isSearch = true">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600" fill="none" viewBox="0 0 24 24"
                  stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -57,8 +87,9 @@ export default {
           </button>
 
           <!-- After clicking search icon -->
-          <div class="relative">
-            <input type="text"
+          <div  v-show="isSearch" class="relative">
+            <input v-model="serchInput"
+                    type="text"
                    class="w-56 rounded-md block border border-gray-300 text-sm px-7 py-1"
                    placeholder="search your task"/>
             <span class="absolute left-1.5 top-2 text-gray-400">
@@ -68,7 +99,7 @@ export default {
                       d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
               </svg>
             </span>
-            <button class="absolute right-1.5 top-2 text-gray-400 hover:text-gray-700">
+            <button @click="closeSerch()" class="absolute right-1.5 top-2 text-gray-400 hover:text-gray-700">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
                    stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
@@ -82,25 +113,26 @@ export default {
 
       <!-- Tasks List -->
       <div class="mt-5 space-y-1">
-        <div class="px-3 py-1.5 bg-indigo-50 hover:bg-indigo-100 rounded flex items-center group">
-          <input type="checkbox" class="w-4 h-4 rounded text-indigo-500 cursor-pointer"/>
-          <span class="text-gray-800 ml-3">I am a task</span>
-          <button class="text-red-500 ml-auto group-hover:block hidden hover:text-red-600">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-            </svg>
+        <!-- Single Task -->
+        <div v-for="(task, index) in tasksList" :key="index"  class="px-3 py-1.5 bg-indigo-50 hover:bg-indigo-100 rounded flex items-center group">
+          <input v-model="task.isComplete" :value="true" type="checkbox" class="w-4 h-4 rounded text-indigo-500 cursor-pointer"/>
+          <span class="text-gray-800 ml-3" :class="{'line-through' :task.isComplete}"> {{task.value}} </span>
+          <button @click="removeTask(index)" class="text-red-500 ml-auto group-hover:block  hover:text-red-600">
+         
+             <h4>Delete</h4>
           </button>
         </div>
-        <div class="text-gray-600 text-center mt-8">
+        <!-- Single Task End -->
+
+        <!-- If No Task -->
+        <div v-if="tasks.length == 0" class="text-gray-600 text-center mt-8">
           You have no tasks :)
         </div>
       </div>
       <!-- Tasks List end -->
 
       <!-- Clear all -->
-      <div class="mt-4">
+      <div @click="clearAll()" class="mt-4">
         <button class="px-6 py-1.5 text-white bg-indigo-500 hover:bg-indigo-600 rounded text-sm">
           Clear all
         </button>
